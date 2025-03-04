8659b37fd95fa4c47aa6fa903a5381ba678d2a9f
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2025-03-03 22:50:50 -0300

    inicio, criação, header, index, stryle

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <?php
+    include 'header.php'
+    ?> 
+     <section id="secao1">
+        
+
+
+     <div class="animated-title">
+  <div class="text-top">
+    <div>
+      <span>Fundação</span>
+    </div>
+  </div>
+  <div class="text-bottom">
+    <div>Essência Azul</div>
+  </div>
+</div>
+
+
+
+        <p id="sobre">Nós da Essência Azul somos uma ONG que apoia e traz auxílio para pessoas autistas e suas famílias, promovendo inclusão e conscientização.</p>
+        <img id= "logo" src="./img/logo.png" alt="logo">
+    </section>
+    
+    <!-- galeria -->
+    <section id="secao2"> 
+        <h2>Galeria de Fotos</h2>
+    </section>
+    <!-- cards -->
+    <section id="secao3">
+        <h2>cards</h2>
+    </section>
+    <!-- entenda sobre -->
+    <section id="secao4">
+
+        <div class="texto">
+            <div class="linha"></div>
+            <p id="entenda">Entenda sobre os documentos de identificação</p>
+            <a href="#" class="botao">clique aqui</a>
+        </div>
+        <div class="imagem">
+            <img src="./img/sobreDocumentos.png">
+        </div>
+    </section>
+    <!-- objetivo -->
+    <section id="secao5">
+        <h2>b</h2>
+    </section>
+    <!-- ajuda -->
+    <section id="secao6">
+        <h2>c</h2>
+    </section>
+
+</body>
+    
+</html>
