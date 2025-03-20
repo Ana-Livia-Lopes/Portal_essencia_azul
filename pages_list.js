1b3ac4a94ed97807aa8c9b80b6a56b1e291da469
Gabriel-Britoo <gabrito343@gmail.com>
2025-03-20 09:57:48 -0300

    mais paginas na lista pages_list e começo da modificação do header

--- pages_list.js
+++ pages_list.js
@@ -20,6 +20,50 @@ module.exports = {
         {
             filelocation: "voluntarios.html",
             pagelocation: "voluntarios"
+        },
+        {
+            filelocation: "recuperar_senha.html",
+            pagelocation: "recuperar_senha"
+        },
+        {
+            filelocation: "gerenciamento.html",
+            pagelocation: "gerenciamento"
+        },
+        {
+            filelocation: "formulario.html",
+            pagelocation: "formulario"
+        },
+        {
+            filelocation: "eventos.html",
+            pagelocation: "eventos"
+        },
+        {
+            filelocation: "entrar.html",
+            pagelocation: [ "login", "entrar" ]
+        },
+        {
+            filelocation: "documentos.html",
+            pagelocation: "documentos"
+        },
+        {
+            filelocation: "diagnostico.html",
+            pagelocation: "diagnostico"
+        },
+        {
+            filelocation: "contato.html",
+            pagelocation: "contato"
+        },
+        {
+            filelocation: "conscientizacao.html",
+            pagelocation: "conscientizacao"
+        },
+        {
+            filelocation: "catalogo.html",
+            pagelocation: "catalogo"
+        },
+        {
+            filelocation: "cadastrar_adm.html",
+            pagelocation: "cadastrar_adm"
         }
     ]
 }
