5c85904cdfa66f2770e0244a7eaa0a6dcaebcd5a
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2025-05-20 14:25:44 -0300

    pobrezinho do lucas

---
+++ components/admin/index_add_imagem.js
@@ -0,0 +1,86 @@
+const { isLogged } = require("../../src");
+
+const content = `
+<button onclick="addImagem()" class="botaoAdd">Adicionar Imagem</button>
+<script>
+    function addImagem() {
+            Swal.fire({
+                title: 'Adicionar imagem',
+                html:\`` + `
+        <div class="modal-container">
+            <form id="upload-image-form">
+                <label for="tituloInput" class="modal-label">Título</label>
+                <input type="text" name="titulo" id="tituloInput" class="modal-input"/>
+                <label for="descricaoInput" class="modal-label">Descrição (opcional)</label>
+                <input type="text" name="descricao" id="descricaoInput" class="modal-input"/>
+                <label for="arquivoInput" class="modal-label">Imagem</label>
+                <input type="file" name="blob" id="arquivoInput" class="modal-file"/>
+            </form>
+        </div>\`
+    ` + `,
+                didOpen() {
+                    document.getElementById("tituloInput").focus();
+                },
+                confirmButtonText: 'Salvar',
+                
+                cancelButtonText: 'Cancelar',
+                showCancelButton: true,
+                focusConfirm: false,
+                customClass: {
+                    popup: 'custom-popup'
+                },
+                preConfirm: () => {
+                    const titulo = document.getElementById("tituloInput");
+                    const descricao = document.getElementById("descricaoInput");
+                    const arquivo = document.getElementById("arquivoInput");
+
+                    const fd = new FormData();
+                    if (!titulo.value.trim()) {
+                        Swal.showValidationMessage("Insira um título.");
+                        return false;
+                    }
+                    if (!arquivo.files[0]) {
+                        Swal.showValidationMessage("Selecione uma imagem.");
+                        return false;
+                    }
+
+                    fd.append("blob", arquivo.files[0]);
+                    fd.append("titulo", titulo.value.trim());
+                    if (descricao.value.trim()) fd.append("descricao", descricao.value.trim());
+
+                    return fd;
+                }
+            }).then(result => {
+                if (result.isConfirmed) {
+                    const formData = result.value;
+                    fetch("/api/imagens", {
+                        method: "POST",
+                        body: formData
+                    }).then(result => result.json()).then(json => {
+                        if (json.status && json.status === "error") {
+                            Swal.fire({
+                                icon: "error",
+                                title: "Erro",
+                                text: json.message,
+                            });
+                        } else {
+                            Swal.fire({
+                                icon: "success",
+                                title: "Sucesso",
+                                text: "Imagem adicionada com sucesso!",
+                            }).then(() => {
+                                window.location.reload();
+                            });
+                        }
+                    })
+                }
+            });
+        }
+</script>`;
+
+module.exports = async function execute(_, { session }) {
+    console.log(session);
+    if (!session) return "";
+    if (!isLogged(session)) return "";
+    return content;
+}
