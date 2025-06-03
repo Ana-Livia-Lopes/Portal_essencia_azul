df41e4f6853d93e5898b6919af5ea70a5c696980
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2025-06-03 01:15:42 -0300

    cadastrar totalmente finalizado

--- components/json/cadastrar_acolhido_preload.js
+++ components/json/cadastrar_acolhido_preload.js
@@ -1,8 +1,10 @@
 const { read, SolicitacaoAcolhido } = require("../../src")
+const { NotFoundError } = require("../../src/server")
 
 module.exports = async function execute(_, { query, session, response }) {
     if (query.id) {
         const acolhido = (await read(session.get("login"), SolicitacaoAcolhido, { id: query.id }, response))[0];
+        if (!acolhido) throw new NotFoundError(response, "Solicitação não encontrada");
         return `
         <script>
             window.SOLICITACAO = ${JSON.stringify(acolhido)};
