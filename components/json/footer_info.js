9797b2f2dbde3dcb2be3bac08b7212768ba53a42
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2025-10-20 20:02:57 -0300

    Correções no link do footer e no preload de solicitações preenchidas pelos usuários onde data não carregava

--- components/json/footer_info.js
+++ components/json/footer_info.js
@@ -2,8 +2,10 @@ const escapeHtml = require("./_escapeHtml.js");
 
 module.exports = function() {
     delete require.cache[require.resolve("../../footer.json")];
-    const footerInfo = require("../../footer.json");
+    const original = require('../../footer.json');
+    const footerInfo = escapeHtml(require("../../footer.json"), false);
+    footerInfo.endereco.link = original.endereco.link;
     return `<script>
-        window.FOOTER_INFO = ${escapeHtml(footerInfo)};
+        window.FOOTER_INFO = ${footerInfo};
     </script>`;
 }
