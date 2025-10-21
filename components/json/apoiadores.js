2b1b0fab4c905b0a7970bf0157f2dea623e14069
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2025-10-21 01:09:35 -0300

    correção de escape com link dos apoiadores

--- components/json/apoiadores.js
+++ components/json/apoiadores.js
@@ -1,8 +1,18 @@
 const { getPublics, Apoiador } = require( "../../src/index.js" );
 const escapeHtml = require("./_escapeHtml.js");
 
+async function processApoiador(apoiador) {
+    let newObj = {};
+    newObj.id = apoiador.id;
+    Object.assign(newObj, apoiador.fields);
+    newObj = escapeHtml(newObj, false);
+    newObj.link = apoiador.fields.link || null;
+    newObj.logo = await apoiador.references.get_logo();
+    return newObj;
+}
+
 module.exports = async function execute() {
     const apoiadores = []
-    for (const apoiador of (await getPublics(Apoiador)).values()) apoiadores.push({ id: apoiador.id, logo: await apoiador.references.get_logo(), ...apoiador.fields });
-    return `<script>window.APOIADORES = ${escapeHtml(apoiadores)}</script>`;
+    for (const apoiador of (await getPublics(Apoiador)).values()) apoiadores.push(await processApoiador(apoiador));
+    return `<script>window.APOIADORES = ${JSON.stringify(apoiadores)}</script>`;
 }
